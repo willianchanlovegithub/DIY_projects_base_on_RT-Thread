6c8399d8ef6d17402e3baa38c406301a52e93eb1
Willian Chan <chentingwei@rt-thread.com>
2019-07-17 11:52:06 +0800

    [First week mission]Add Projects and README.md
    Signed-off-by: Willian Chan <chentingwei@rt-thread.com>

---
+++ DIY1/First_week_mission/stm32f407-atk-explorer/applications/main.c
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-06     SummerGift   first version
+ * 2018-11-19     flybreak     add stm32f407-atk-explorer bsp
+ * 2019-07-15     WillianCham  DIY Demo(First week mission)
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+#include "sensor.h"
+#include "sensor_dallas_ds18b20.h"
+
+#define LED0_PIN         GET_PIN(F, 9)
+#define DS18B20_DATA_PIN GET_PIN(G, 9)
+
+static void read_temp_entry(void *parameter)
+{
+    rt_device_t dev = RT_NULL;
+    struct rt_sensor_data sensor_data;
+    rt_size_t res;
+
+    dev = rt_device_find(parameter);
+    if (dev == RT_NULL)
+    {
+        rt_kprintf("Can't find device:%s\n", parameter);
+        return;
+    }
+
+    if (rt_device_open(dev, RT_DEVICE_FLAG_RDWR) != RT_EOK)
+    {
+        rt_kprintf("open device failed!\n");
+        return;
+    }
+    rt_device_control(dev, RT_SENSOR_CTRL_SET_ODR, (void *)100);
+
+    while (1)
+    {
+        res = rt_device_read(dev, 0, &sensor_data, 1);
+        if (res != 1)
+        {
+            rt_kprintf("read data failed!size is %d\n", res);
+            rt_device_close(dev);
+            return;
+        }
+        else
+        {
+            rt_kprintf("temp:%3d.%dC, timestamp:%5d\n", sensor_data.data.temp / 10, sensor_data.data.temp % 10, sensor_data.timestamp);
+        }
+        rt_thread_mdelay(100);
+    }
+}
+
+static void led_shine_entry(void *parameter)
+{
+    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+    
+    while(1)
+    {
+        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_thread_mdelay(500);
+    }
+}
+
+int main(void)
+{
+    rt_thread_t ds18b20_thread, led_thread;
+    
+    ds18b20_thread = rt_thread_create("18b20tem", read_temp_entry, "temp_ds18b20",
+                                      512, RT_THREAD_PRIORITY_MAX / 2, 20);
+    if (ds18b20_thread != RT_NULL)
+    {
+        rt_thread_startup(ds18b20_thread);
+    }
+    
+    led_thread = rt_thread_create("ledshine", led_shine_entry, RT_NULL,
+                                  192, RT_THREAD_PRIORITY_MAX / 2, 20);
+    if (led_thread != RT_NULL)
+    {
+        rt_thread_startup(led_thread);
+    }
+    
+    return RT_EOK;
+}
+
+static int rt_hw_ds18b20_port(void)
+{
+    struct rt_sensor_config cfg;
+    
+    cfg.intf.user_data = (void *)DS18B20_DATA_PIN;
+    rt_hw_ds18b20_init("ds18b20", &cfg);
+    
+    return RT_EOK;
+}
+INIT_ENV_EXPORT(rt_hw_ds18b20_port);
