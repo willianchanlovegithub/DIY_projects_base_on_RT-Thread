d1668414a9b232a00fbcc183a1564a698f56f079
Willian Chan <chentingwei@rt-thread.com>
2019-07-31 00:09:21 +0800

    [Third week mission]Additional optimization algorithm
    Signed-off-by: Willian Chan <chentingwei@rt-thread.com>

--- DIY1/Third_week_mission/RECEIVE(stm32l475-atk-pandora)(SPI_Flash)/applications/main.c
+++ DIY1/Third_week_mission/RECEIVE(stm32l475-atk-pandora)(SPI_Flash)/applications/main.c
@@ -7,7 +7,7 @@
  * Date           Author       Notes
  * 2018-11-06     SummerGift   first version
  * 2019-07-23     WillianChan  DIY Demo2(Second week mission)
- * 2019-07-27     WillianChan  DIY Demo3(Third week mission)
+ * 2019-07-30     WillianChan  DIY Demo3(Third week mission)
  */
 
 #include <rtthread.h>
@@ -15,8 +15,6 @@
 #include <board.h>
 #include <stdio.h>
 #include <string.h>
-#include <dfs_posix.h>
-#include <rtdbg.h>
 #include "drv_spi.h"
 #include "nrf24l01.h"
 
@@ -24,22 +22,21 @@
 #define NRF24L01_CE_PIN     GET_PIN(D, 4)
 #define NRF24_IRQ_PIN       GET_PIN(D, 3)
 #define NRF24L01_SPI_DEVICE "spi20"
+#define WRITE_EVENT         (0x01U << 0)  /* 感兴趣的事件 */
 
-#define MP_BLOCK_SIZE       RT_ALIGN(sizeof(struct tmp_msg), sizeof(intptr_t)) /* 为了字节对齐 */
-#define MB_LEN              (4)
-#define MP_LEN              MB_LEN
+static rt_event_t recvdata_event;         /* 事件集 */
+static struct rt_ringbuffer *recvdatabuf; /* ringbuffer */
 
-/* 这是要传输的数据结构定义 */
-struct tmp_msg
+struct recvdata
 {
-    char str_value[32];
+    int timestamp;
+    float temperature;
 };
-static rt_mailbox_t tmp_msg_mb; /* 邮箱 */
-static rt_mp_t tmp_msg_mp;      /* 内存池 */
 
 static void nrf24l01_receive_entry(void *parameter)
 {
-    struct tmp_msg *msg;
+    struct recvdata buf;
+    static char str_data[64];
     struct hal_nrf24l01_port_cfg halcfg;
     nrf24_cfg_t cfg;
 
@@ -53,40 +50,72 @@ static void nrf24l01_receive_entry(void *parameter)
 
     while (1)
     {
-        rt_thread_mdelay(90);
-        
         if (!rx_pipe_num_choose())
         {
-            /* 申请一块内存 要是内存池满了 就挂起等待 */
-            msg = rt_mp_alloc(tmp_msg_mp, RT_WAITING_FOREVER);
-            rt_strncpy(msg->str_value, (char *)RxBuf_P0, sizeof msg->str_value);
-            rt_mb_send(tmp_msg_mb, (rt_ubase_t)msg);
-            msg = NULL;
+            if(sscanf((char *)RxBuf_P0, "%d,+%f", &buf.timestamp, &buf.temperature) != 2)
+            {
+                if(sscanf((char *)RxBuf_P0, "%d,-%f", &buf.timestamp, &buf.temperature) != 2)
+                {
+                    continue;
+                }
+                buf.temperature = -buf.temperature;
+            }
+            sprintf(str_data, "%d,%f\n",buf.timestamp, buf.temperature);
+            
+            rt_ringbuffer_put(recvdatabuf, (rt_uint8_t *)str_data, strlen(str_data));
+            rt_event_send(recvdata_event, WRITE_EVENT);
         }
+        
+        rt_thread_mdelay(30);
     }
 }
 
 static void save_recv_data_entry(void *parameter)
 {
     FILE *recvdata_p0 = RT_NULL;
-    struct tmp_msg *msg;
-    
+    rt_uint32_t set;
+    static int writebuffer[1024];
+    rt_size_t size;
     while (1)
     {
-        rt_thread_mdelay(100);
-        
-        if (rt_mb_recv(tmp_msg_mb, (rt_ubase_t *)&msg, RT_WAITING_FOREVER) == RT_EOK)
+        if (rt_event_recv(recvdata_event, WRITE_EVENT, RT_EVENT_FLAG_OR | RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &set) != RT_EOK)
         {
-            recvdata_p0 = fopen("recvdata_p0.txt", "a+");
+            continue;
+        }
+        
+        do
+        {
+            if (rt_event_recv(recvdata_event, WRITE_EVENT, RT_EVENT_FLAG_OR | RT_EVENT_FLAG_CLEAR, rt_tick_from_millisecond(1000), &set) == RT_EOK)
+            {
+                if (rt_ringbuffer_data_len(recvdatabuf) > (4096 / 2))
+                {
+                    recvdata_p0 = fopen("recvdata_p0.csv", "ab+");
+                    
+                    if (recvdata_p0 != RT_NULL)
+                    {
+                        while(rt_ringbuffer_data_len(recvdatabuf))
+                        {
+                            size = rt_ringbuffer_get(recvdatabuf, (rt_uint8_t *)writebuffer, (4096 / 2));
+                            fwrite(writebuffer, 1, size, recvdata_p0);
+                        }
+                        fclose(recvdata_p0);
+                    }
+                }
+                continue;
+            }
+            recvdata_p0 = fopen("recvdata_p0.csv", "ab+");
+
             if (recvdata_p0 != RT_NULL)
             {
-                fputs(msg->str_value, recvdata_p0);
-                fputs("\n", recvdata_p0);
+                while(rt_ringbuffer_data_len(recvdatabuf))
+                {
+                    size = rt_ringbuffer_get(recvdatabuf, (rt_uint8_t *)writebuffer, (4096 / 2));
+                    fwrite(writebuffer, 1, size, recvdata_p0);
+                }
                 fclose(recvdata_p0);
             }
-            rt_mp_free(msg); /* 释放内存块 */
-            msg = RT_NULL;   /* 请务必要做 */
-        }
+            
+        } while(0);
     }
 }
 
@@ -107,8 +136,10 @@ int main(void)
 {
     rt_thread_t nrf24l01_thread, led_thread, DFS_thread;
     
-    tmp_msg_mb = rt_mb_create("temp_mb0", MB_LEN, RT_IPC_FLAG_FIFO);
-    tmp_msg_mp = rt_mp_create("temp_mp0", MP_LEN, MP_BLOCK_SIZE);
+    recvdata_event = rt_event_create("temp_evt0", RT_IPC_FLAG_FIFO);
+    RT_ASSERT(recvdata_event);
+    recvdatabuf = rt_ringbuffer_create(4096); /* ringbuffer的大小是4KB */
+    RT_ASSERT(recvdatabuf);
     
     nrf24l01_thread  = rt_thread_create("nrfrecv", nrf24l01_receive_entry, RT_NULL,
                                         1024, RT_THREAD_PRIORITY_MAX / 2, 20);
